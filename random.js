5c561a8b00d410e984a372169b0028bf49ef9484
IFAIZC <165414147+IFAIZC@users.noreply.github.com>
2024-11-20 22:53:54 +0800

    added note for future changes

--- random.js
+++ random.js
@@ -1,11 +1,15 @@
-// an array to list out 1-10
+// an array to list out 1-3
 const number = Array.from({ length: 3 }, (_, i) => i + 1);
 
 // to generate random number
 const randomNumber = number[Math.floor(Math.random() * number.length)];
 
-let userInput = 2;
+//using document.getElementById is kind of like linking the html <p> tag to Javscript, to display output.
+const showResult = document.getElementById("randomNumberResult");
 
+let userInput = 2; //to be deleted soon
+
+// to add user input function, and rewrite proper code for this part below.
 function checkNumber(userInput, randomNumber) {
     if (userInput === randomNumber) {
         showResult.textContent = "You guessed it RIGHT!";
@@ -13,10 +17,7 @@ function checkNumber(userInput, randomNumber) {
         showResult.textContent = "You guessed it WRONG!";
     }
 }
-
-//using document.getElementById is kind of like linking the html <p> tag to Javscript, to display output.
-const showResult = document.getElementById("randomNumberResult");
-
+// to add user input function, and rewrite proper code for this part above.
 
 console.log("User : " + userInput);
 console.log("Computer : " + randomNumber);
